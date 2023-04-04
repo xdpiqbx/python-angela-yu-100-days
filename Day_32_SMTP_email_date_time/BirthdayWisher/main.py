9476a92db1b6cffb7661832fd07aaacc1f71e627
xdpiqbx <xdpiqbx@gmail.com>
2023-04-04 19:59:24 +0300

    added day 32

--- Day_32_SMTP_email_date_time/BirthdayWisher/main.py
+++ Day_32_SMTP_email_date_time/BirthdayWisher/main.py
@@ -1,14 +1,40 @@
-##################### Extra Hard Starting Project ######################
+import random
+from datetime import datetime
 
-# 1. Update the birthdays.csv
+import pandas
 
-# 2. Check if today matches a birthday in the birthdays.csv
+BIRTHDAYS_CSV_FILE = "./birthdays.csv"
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with
-# the person's actual name from birthdays.csv
+LETTERS = [
+    "./letter_templates/letter_1.txt",
+    "./letter_templates/letter_2.txt",
+    "./letter_templates/letter_3.txt"
+]
 
-# 4. Send the letter generated in step 3 to that person's email address.
+birthdays_dict = pandas.read_csv(BIRTHDAYS_CSV_FILE).to_dict(orient="records")
 
+def is_today(birthday, today):
+    return birthday["day"] == today.day and birthday["month"] == today.month
 
+def send_email(subject, body, sender, recipients, password):
+    return {
+        "subject": subject,
+        "body": body,
+        "sender": sender,
+        "recipients": recipients,
+        "password": password
+    }
 
+birthdays_today = [birthday for birthday in birthdays_dict if is_today(birthday, datetime.now())]
 
+for birthday in birthdays_today:
+    with open(random.choice(LETTERS)) as letter_file:
+        content = letter_file.read().replace("[NAME]", birthday["name"])
+
+        print(send_email(
+            f"Happy Birthday {birthday['name']}",
+            content,
+            "my_email@mail.com",
+            birthday['email'],
+            "p@ssworD"
+        ))
