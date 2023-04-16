78ce4317bebc9583c257cedc55a6c0edf18d0811
xdpiqbx <xdpiqbx@gmail.com>
2023-04-16 22:52:14 +0300

    added header

--- Day_37_HTTP/habit-tracker/main.py
+++ Day_37_HTTP/habit-tracker/main.py
@@ -1,16 +1,33 @@
+import requests
+from dotenv import dotenv_values
+
+env = dotenv_values("../../.env")
+
+pixela_endpoint = "https://pixe.la/v1/users"
+username = env['PIXELA_USER_NAME']
+TOKEN = env['PIXELA_API_KEY']
+
 # ---------------------------------- Do it ONE time to create user ----------------------------------
-# import requests
-# from dotenv import dotenv_values
-#
-# env = dotenv_values("../../.env")
-#
-# pixela_endpoint = "https://pixe.la/v1/users"
-#
 # user_params = {
-#     "token": env['PIXELA_API_KEY'],
-#     "username": env['PIXELA_USER_NAME'],
+#     "token": TOKEN,
+#     "username": username,
 #     "agreeTermsOfService": "yes",
 #     "notMinor": "yes",
 # }
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{username}/graphs"
+graph_config = {
+    "id": "graph1",
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai"
+}
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# https://pixe.la/v1/users/xdpiqbx/graphs/graph1.html
+print(graph_endpoint)
