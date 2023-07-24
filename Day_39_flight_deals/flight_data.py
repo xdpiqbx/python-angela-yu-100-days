e9f3bacf290ed07e9065e6589772d7ac83c7427e
xdpiqbx <xdpiqbx@gmail.com>
2023-07-24 23:48:19 +0300

    flight deals in process

--- Day_39_flight_deals/flight_data.py
+++ Day_39_flight_deals/flight_data.py
@@ -1,3 +1,87 @@
+import os
+import json
+import requests
+from datetime import datetime as dt, timedelta
+from dateutil.relativedelta import relativedelta
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+FLIGHT_DATA_JSON = "./resources/flight_data.json"
+tequila_kivi_url = "https://api.tequila.kiwi.com/v2/search"
+iso_8601_format = '%Y-%m-%dT%H:%M:%S.%fZ'
+tomorrow = dt.now() + timedelta(days=1)
+
+def create_json_with_empty_structure() -> dict:
+    print("create_json_with_empty_structure")
+    with open(FLIGHT_DATA_JSON, 'w') as json_file:
+        empty_data = {"last_update": ""}
+        json.dump(empty_data, json_file, indent=4)
+        return empty_data
+
+
+def try_to_get_flight_data_json() -> dict:
+    print("try_to_get_flight_data_json")
+    try:
+        with open(FLIGHT_DATA_JSON) as json_file:
+            return json.load(json_file)
+    except (json.JSONDecodeError, FileNotFoundError):
+        return create_json_with_empty_structure()
+
+
 class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
+    #  This class is responsible for structuring the flight data.
+    def __init__(self):
+        print("__init__")
+        self.__sheety_prices = list()
+        self.__sheet_ids_iata_codes = list()
+        self.__flight_data = dict()
+        self.__headers = {"apikey": os.environ["KIWI_API"]}
+
+    def set_sheety_prices(self, sheety_prices):
+        print("set_sheety_prices")
+        self.__sheety_prices = sheety_prices
+
+    def __request_flight_data(self):
+        print("__request_flight_data")
+        flight_data = dict()
+        flight_data['last_update'] = dt.now().strftime('%d/%m/%Y')
+        for city in self.__sheety_prices:
+            params = {
+                'accept': 'application/json',
+                'fly_from': 'LON',  # London[LON] # Poland: Warsaw[WAW] | Rzeszów[RZE] | Kraków[KRK]
+                'fly_to': city['iataCode'],
+                'date_from': tomorrow.strftime('%d/%m/%Y'),
+                'date_to': (tomorrow + relativedelta(months=6)).strftime('%d/%m/%Y'),
+                'selected_cabins': 'M',  # M (economy), W (economy premium), C (business), or F (first class)
+                'max_stopovers': 0,  # 0 it means direct flight
+                'nights_in_dst_from': 6,
+                'nights_in_dst_to': 13,
+                'curr': 'GBP',
+                'price_to': city['lowestPrice'],  # 'price_from': 800,
+                # 'sort': 'price',
+                'sort': 'date',
+                # 'limit': 1
+            }
+            response = requests.get(url=tequila_kivi_url, params=params, headers=self.__headers)
+            flight_data[city['city']] = response.json()
+        return flight_data
+
+    def init_flight_data(self):
+        print('init_flight_data')
+        self.__set_flight_data(try_to_get_flight_data_json())
+        if self.__flight_data.get('last_update') != dt.now().strftime('%d/%m/%Y'):
+            print("last_update FALSE !")
+            result = self.__request_flight_data()
+            self.__set_flight_data(result)
+            with open(FLIGHT_DATA_JSON, 'w') as json_file:
+                json.dump(self.__flight_data, json_file, indent=4)
+
+    def __set_flight_data(self, flight_data):
+        print("__set_flight_data")
+        self.__flight_data = flight_data
+
+    def get_flight_data(self):
+        print("get_flight_data")
+        return self.__flight_data
--- Day_39_flight_deals/main_raw.py
+++ Day_39_flight_deals/main_raw.py
@@ -1,82 +1,61 @@
 # This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes
 # to achieve the program requirements.
 
-import os
 import json
-from time import sleep
-import requests
-from datetime import datetime as dt, timedelta
+from datetime import datetime as dt
 from dateutil.relativedelta import relativedelta
 
 from data_manager import DataManager
 from flight_search import FlightSearch
+from flight_data import FlightData
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# === GET cities from "https://api.sheety.co" and save to json_file
 d_manager = DataManager()
 sheety_prices = d_manager.get_current_prices_data()
 
 # ==== convert data from json to generator for using in tequila_kivi
-cities = ({'id': city['id'], 'city': city['city']} for city in sheety_prices)
+# cities = ({'id': city['id'], 'city': city['city']} for city in sheety_prices)
+#
+# flight_s = FlightSearch()
+# flight_s.request_iata_codes_for_cities(cities)
+# sheet_ids_iata_codes = flight_s.get_ids_and_iata_codes_for_sheety()
 
-flight_s = FlightSearch()
-flight_s.request_iata_codes_for_cities(cities)
-sheet_ids_iata_codes = flight_s.get_ids_and_iata_codes_for_sheety()
-
-# === PUT iataCodes to SHEETY
-d_manager.put_iata_codes_to_sheety(sheet_ids_iata_codes)
+# d_manager.put_iata_codes_to_sheety(sheet_ids_iata_codes)
 
 # === https://api.tequila.kiwi.com
-# iso_8601_format = '%Y-%m-%dT%H:%M:%S.%fZ'
-# tequila_kivi_url = "https://api.tequila.kiwi.com/v2/search"
-# headers = {"apikey": os.environ["KIWI_API"]}
-# tomorrow = dt.now() + timedelta(days=1)
-# # cheapest fly to the next 6 month
-# to_city_flights = dict()
-# for city in cities_dict['prices']:
-#     flights = list()
-#     params = {
-#         'accept': 'application/json',
-#         'fly_from': 'LON',  # London[LON] # Poland: Warsaw[WAW] | Rzeszów[RZE] | Kraków[KRK]
-#         'fly_to': city['iataCode'],
-#         'date_from': tomorrow.strftime('%d/%m/%Y'),
-#         'date_to': (tomorrow + relativedelta(months=6)).strftime('%d/%m/%Y'),
-#         'selected_cabins': 'M',  # M (economy), W (economy premium), C (business), or F (first class)
-#         'max_stopovers': 0,  # 0 it means direct flight
-#         'nights_in_dst_from': 6,
-#         'nights_in_dst_to': 13,
-#         'curr': 'GBP',
-#         'price_to': city['lowestPrice'],  # 'price_from': 800,
-#         'sort': 'date',
-#         'limit': 1
-#     }
-#     response = requests.get(url=tequila_kivi_url, params=params, headers=headers)
-#     all_fly_data = response.json()["data"]
-#
-#     price = city['lowestPrice']
-#     for fly_data in all_fly_data:
-#         if price < fly_data['conversion']['GBP']:
-#             price = fly_data['conversion']['GBP']
-#             continue
-#         return_home_date = dt.strptime(fly_data['local_arrival'], iso_8601_format) + relativedelta(days=fly_data['nightsInDest'])
-#         flights.append({
-#             'cityFrom': fly_data['cityFrom'],
-#             'flyFrom': fly_data['flyFrom'],
-#             'cityTo': fly_data['cityTo'],
-#             'flyTo': fly_data['flyTo'],
-#             'price_gbp': fly_data['conversion']['GBP'],  # Price
-#             'nightsInDest': fly_data['nightsInDest'],
-#             'local_departure': fly_data['local_departure'],
-#             'local_arrival': fly_data['local_arrival'],
-#             'return_home': return_home_date.strftime(iso_8601_format),
-#             'total_duration': fly_data['duration']['total'],
-#             'virtual_interlining': fly_data['virtual_interlining']
-#         })
-#     to_city_flights[city['city']] = flights
-# print(to_city_flights)
+fd = FlightData()
+fd.set_sheety_prices(sheety_prices)
+fd.init_flight_data()
+raw_flight_data = fd.get_flight_data()
+
+
+iso_8601_format = '%Y-%m-%dT%H:%M:%S.%fZ'
+to_city_flights = dict()
+for city in sheety_prices:
+    flights = list()
+    price = city['lowestPrice']
+    for fly_data in raw_flight_data[city['city']]['data']:
+        return_home_date = dt.strptime(fly_data['local_arrival'], iso_8601_format) + relativedelta(days=fly_data['nightsInDest'])
+        flights.append({
+            'cityFrom': fly_data['cityFrom'],
+            'flyFrom': fly_data['flyFrom'],
+            'cityTo': fly_data['cityTo'],
+            'flyTo': fly_data['flyTo'],
+            'price_gbp': fly_data['conversion']['GBP'],  # Price
+            'nightsInDest': fly_data['nightsInDest'],
+            'local_departure': fly_data['local_departure'],
+            'local_arrival': fly_data['local_arrival'],
+            'return_home': return_home_date.strftime(iso_8601_format),
+            'total_duration': fly_data['duration']['total'],
+            'virtual_interlining': fly_data['virtual_interlining']
+        })
+    to_city_flights[city['city']] = flights
+
+with open("./resources/result.json", 'w') as json_file:
+    json.dump(to_city_flights, json_file, indent=4)
 
 
 # for item in to_city_flights:
