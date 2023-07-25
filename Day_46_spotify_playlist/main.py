8aff9bb7a3c4c94a6a051dc915a640d10538fbe5
xdpiqbx <xdpiqbx@gmail.com>
2023-07-25 23:50:15 +0300

    get data from billboard done

--- Day_46_spotify_playlist/main.py
+++ Day_46_spotify_playlist/main.py
@@ -1,18 +1,44 @@
+import os
 from bs4 import BeautifulSoup
 import requests
+import json
+from pathlib import Path
+
+SAVE_TO = "./already_requested/"
+data_must_be_requested = True
 
 date = input("Which year do you want to travel to? Type the date in this format DD.MM.YYYY : ")
 day, month, year = date.split('.')
 
-url = f"https://www.billboard.com/charts/hot-100/{year}-{month}-{day}/"
-billboard_html = "./resources/billboard.html"
+# TODO: check data! if it is future change to today
 
-response = requests.get(url)
-with open(billboard_html, "w", encoding="utf8") as file:
-    file.write(response.text)
+FILENAME = f"{day}_{month}_{year}.json"
 
-with open(billboard_html, encoding="utf8") as file:
-    soup = BeautifulSoup(file, 'html.parser')
+for filename in os.listdir(SAVE_TO):
+    if filename == FILENAME:
+        data_must_be_requested = False
+        break
 
+if data_must_be_requested:
+    print("Data was requested")
+    url = f"https://www.billboard.com/charts/hot-100/{year}-{month}-{day}/"
 
-# titles = soup.select('.....')
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    songs = soup.select('li.o-chart-results-list__item:has(h3)')
+    board = list()
+    for i, song in enumerate(songs, start=1):
+        board.append({
+            "place": i,
+            "title": song.select_one('h3').getText().strip(),
+            "group": song.select_one('span').getText().strip()
+        })
+
+    with open(Path(SAVE_TO, FILENAME), 'w') as json_file:
+        json.dump(board, json_file, indent=4)
+
+with open(Path(SAVE_TO, FILENAME)) as json_file:
+    hot_100 = json_file.read()
+
+print(hot_100)
