a8ed59cd34d74386c3ac2f0982504a135e2da7c6
xdpiqbx <xdpiqbx@gmail.com>
2023-08-19 17:28:47 +0300

    added day 61

--- Day_61_flask_wtf/login_form.py
+++ Day_61_flask_wtf/login_form.py
@@ -1,14 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Length
 
-class MyForm(FlaskForm):
+
+class LoginForm(FlaskForm):
     email = StringField(
         label='Email',
-        validators=[DataRequired()]
+        default=None,
+        validators=[
+            DataRequired(message="Email required."),
+            Email(message="Invalid Email address")
+        ]
     )
     password = PasswordField(
         label='Password',
-        validators=[DataRequired()],
+        validators=[
+            DataRequired(message="Email required."),
+            Length(min=8, message="Password must be at least 8 characters long")
+        ],
     )
     submit = SubmitField(label="Log In")
--- Day_61_flask_wtf/main.py
+++ Day_61_flask_wtf/main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-from login_form import MyForm
+from flask_bootstrap import Bootstrap5
+from flask import Flask, render_template, request
+from login_form import LoginForm
 
 SECRET_KEY = '''
 Red underlines? Install the required packages first: 
@@ -12,15 +13,30 @@ pipenv install -r ./Day_61_flask_wtf/requirements.txt
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
 
+bootstrap = Bootstrap5(app)
+
+
 @app.route("/")
 def home():
     return render_template('index.html')
 
+
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    form = MyForm()
-    form.validate_on_submit()
-    return render_template('login.html', form=form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        is_email_correct = form.email.data == 'admin@email.com'
+        is_password_correct = form.password.data == '12345678'
+        if is_email_correct and is_password_correct:
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
+
+    return render_template(
+        # 'login.html',
+        'login_bootstrap_flask.html',
+        form=form
+    )
 
 
 if __name__ == '__main__':
